6571b05207e2da40baa52d5e6ae904a7b82894f5
Vespid <shadifox@gmail.com>
2017-04-04 21:55:09 -0700

    Create hw1.c

---
+++ hw1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+main()
+{
+        printf("\nPart A:\n\nI am writing my first C program.\nI am writing my first C program.\nI am writing my first C program.\n\n\nPart B:\n\nIt represents my hw1 C assignment.  It represents my hw1 C assignment.\n\n");
+}
